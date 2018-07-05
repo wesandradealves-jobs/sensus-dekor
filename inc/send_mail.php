787f947cee00859738addee6663460d42a37713b
Wesley Alves <wesandradealves@gmail.com>
2018-07-05 13:38:01 -0300

    E-mail pessoal adicionado para teste de envio

--- inc/send_mail.php
+++ inc/send_mail.php
@@ -1,49 +1,64 @@
 <?php
- 
-$emailsender = "contato@" . $_SERVER[HTTP_HOST];
- 
-/* Verifica qual �o sistema operacional do servidor para ajustar o cabe�alho de forma correta.  */
-if(PATH_SEPARATOR == ";") $quebra_linha = "\r\n"; //Se for Windows
-else $quebra_linha = "\n"; //Se "não for Windows"
- 
-// Passando os dados obtidos pelo formul�rio para as vari�veis abaixo
-$nomeremetente     = $_POST['user_nome'];
-$emailremetente    = $_POST['user_email'];
-$emaildestinatario = $emailsender;
-$comcopia          = 'wesandradealves@gmail.com';
-$assunto           = 'Contato Site';
-$mensagem          = $_POST['user_msg'];
- 
- 
-/* Montando a mensagem a ser enviada no corpo do e-mail. */
-$mensagemHTML = '<P>Esse email &eacute; um teste enviado no formato HTML via PHP mail();!</P>
-<P>Aqui est� a mensagem postada por voc�; formatada em HTML:</P>
-<p><b><i>'.$mensagem.'</i></b></p>
-<hr>';
- 
- 
-/* Montando o cabeçalho da mensagem */
-$headers = "MIME-Version: 1.1" .$quebra_linha;
-$headers .= "Content-type: text/html; charset=iso-8859-1" .$quebra_linha;
-// Perceba que a linha acima cont�m "text/html", sem essa linha, a mensagem n�o chegar� formatada.
-$headers .= "From: " . $emailsender.$quebra_linha;
-$headers .= "Cc: " . $comcopia . $quebra_linha;
-$headers .= "Reply-To: " . $emailremetente . $quebra_linha;
-// Note que o e-mail do remetente ser� usado no campo Reply-To (Responder Para)
- 
-/* Enviando a mensagem */
+session_start();
 
-//� obrigat�rio o uso do par�metro -r (concatena��o do "From na linha de envio"), aqui na Locaweb:
+if (!empty($_POST['user_email']) && !empty($_POST['user_nome']) && !empty($_POST['user_msg'])) {
+	
+	if (filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+		$name = $_POST['user_nome'];
+		$_SESSION['name'] = $name;
+		$email = $_POST['user_email'];
+		$msg = $_POST['user_msg'];
+		$email_servidor = "contato@sensusdekor.com.br";
+		$to = "wesandradealves@gmail.com";
+		$subject = "Contato pelo Site";
+		$date_send = date('d/m/Y');
+		$hour_send = date('H:i');
+		$template = "
+			
 
-if(!mail($emaildestinatario, $assunto, $mensagemHTML, $headers ,"-r".$emailsender)){ // Se for Postfix
-    $headers .= "Return-Path: " . $emailsender . $quebra_linha; // Se "n�o for Postfix"
-    mail($emaildestinatario, $assunto, $mensagemHTML, $headers );
+		<html>
+		<body style='background:#F2F2F2; max-width:600px; margin:auto; font-family:'Gill Sans', 'Gill Sans MT', 'Myriad Pro', 'DejaVu Sans Condensed', 'Helvetica', 'Arial', sans-serif; '>	
+			<div class='container' style='background:#FFF; width:90%; margin:auto; padding:20px'>
+		    <div class='logo'><img src='http://www.sensusdekor.com.br/assets/imgs/logo.svg' style='width:150px'></div>
+				<div class='header'>
+					<h2 style='font-size:1.6em'><span style='color:#f19200'>$name</span> enviou a seguinte mensagem:</h2>
+					<p><b>Data de envio:</b> $date_send <br> <b>Hora de envio:</b> $hour_send </p>
+				</div>
+				<div class='msg' style='background:#002239; color:#fff; border-radius:10px; padding:20px; margin-top:20px;'>
+					$msg
+				</div>		
+			</div>
+		</body>		
+		</html>
+
+		";		
+
+		$headers = "From: $email_servidor\r\n" .
+               "Reply-To: $email\r\n" .
+               "X-Mailer: PHP/" . phpversion() . "\r\n";
+	    $headers .= "MIME-Version: 1.0\r\n";
+	    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+	    if (mail($to, $subject, nl2br($template), $headers)) {
+	    	$_SESSION['msg'] = "Mensagem enviada com sucesso! <br> Obrigado pelo seu contato. <br> Responderemos em breve.";
+	    	$_SESSION['type'] = "success";
+	    	header('Location:http://www.sensusdekor.com.br/contato.html');	    	
+	    } else {
+	    	$_SESSION['msg'] = "Ocorreu um erro. :( <br> Por algum motivo a mensagem não pode ser enviada. <br> Por favor tente novamente em alguns minutos, caso o erro persista contacte-nos por telefone. Sentimos muito pelo ocorrido. <br> :(";	
+	    	$_SESSION['type'] = "mistakes";    	
+	    	header('Location:http://www.sensusdekor.com.br/contato.html');
+	    }   
+
+
+	} else {
+		$_SESSION['msg'] = "O e-mail informado não é válido. <br> Por gentileza, verifique o e-mail e tente novamente.";
+		$_SESSION['type'] = "mistakes";		
+		header('Location:http://www.sensusdekor.com.br');
+	}
+
+} else {
+	header('Location:http://www.sensusdekor.com.br/contato.html');
 }
- 
-/* Mostrando na tela as informa��es enviadas por e-mail */
-print "Mensagem <b>$assunto</b> enviada com sucesso!<br><br>
-De: $emailsender<br>
-Para: $emaildestinatario<br>
-Com c�pia: $comcopia<br>
-<p><a href='".$_SERVER["HTTP_REFERER"]."'>Voltar</a></p>"
+
+
 ?>
